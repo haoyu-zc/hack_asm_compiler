e2482f5764aa49f88c9bbe1df488584e06120022
ikspike <clifford.k.zhang@outlook.com>
2020-06-16 22:04:30 +0800

    bug: C-Command translation is incomplete
    ❯ .\a.exe .\Add.asm
    0000000000000010
    111100010000
    0000000000000011
    1110000010010000
    0000000000000000
    111010001000

--- Main.cpp
+++ Main.cpp
@@ -4,23 +4,36 @@
 #include "Code.h"
 #include <stdexcept>
 
-using std::string;
 using std::invalid_argument;
+using std::string;
 
 int main(int argc, char *argv[])
 {
-    if(!argv[1])
+    if (!argv[1])
     {
         throw invalid_argument("ERROR. Assembly file name not provided!");
     }
     string asmFile = argv[1];
     Code code;
     Parser parser(asmFile);
-    // Output coded hack file.
-    // ofstream hackFile;
-    // hackFile.open("output.hack");
-    // hackFile << "To be or not to be.\n";
-    // hackFile.close();
+    
+    string command;
+    string binary;
+    while (parser.hasMoreCommands())
+    {
+        parser.advance();
+        command = parser.currentCmd;
+        if (parser.commandType() == A_COMMAND)
+        {
+            binary = code.aCmdCode(parser.symbol());
+            cout << binary << endl;
+        }
+        else if (parser.commandType() == C_COMMAND)
+        {
+            binary = "111" + code.comp(parser.comp()) + code.dest(parser.dest()) + code.jump(parser.jump());
+            cout << binary << endl;
+        }
+    }
 
     return 0;
 }
