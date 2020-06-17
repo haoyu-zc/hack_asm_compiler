792f62b68e5a53a4833936084af9d77b8624f8c2
ikspike <clifford.k.zhang@outlook.com>
2020-06-17 18:18:13 +0800

    bug: Translation failed

--- Main.cpp
+++ Main.cpp
@@ -3,11 +3,17 @@
 #include <string>
 #include "Parser.h"
 #include "Code.h"
+#include "SymbolTable.h"
 #include <stdexcept>
+#include <algorithm>
 
-using std::invalid_argument;
-using std::string;
-using std::ofstream;
+using namespace std;
+
+bool isNumber(const string& s)
+{
+    return !s.empty() && std::find_if(s.begin(), 
+        s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
+}
 
 int main(int argc, char *argv[])
 {
@@ -15,27 +21,61 @@ int main(int argc, char *argv[])
     {
         throw invalid_argument("ERROR. Assembly file name not provided!");
     }
-
     string asmFile = argv[1];
+
+    SymbolTable symbTable;
+    // First scan to deal with labels.
+    Parser parser1(asmFile);
+    // Rom addresss starting from 0. ++romAdd when encounter none-L commands.
+    int romAddr = 0;
+    while (parser1.hasMoreCommands())
+    {
+        parser1.advance();
+        if (parser1.commandType() == L_COMMAND)
+            symbTable.addEntry(parser1.symbol(), romAddr + 1);
+        else
+            romAddr++;
+    }
+
+    // Second scan.
     ofstream hackFile;
     hackFile.open(asmFile.substr(0, asmFile.find('.')) + ".hack");
-    Code code;
-    Parser parser(asmFile);
-    
+    int memOffset = 16;
+    int memAddr = 0;
     string command;
+    string symbol;
     string binary;
-    while (parser.hasMoreCommands())
+    Parser parser2(asmFile);
+    Code code;
+    while (parser2.hasMoreCommands())
     {
-        parser.advance();
-        command = parser.currentCmd;
-        if (parser.commandType() == A_COMMAND)
+        parser2.advance();
+        command = parser2.currentCmd;
+        if (parser2.commandType() == A_COMMAND)
         {
-            binary = code.aCmdCode(parser.symbol());
-            hackFile << binary << endl;
+            symbol = parser2.symbol();
+            // @ a constant
+            if (isNumber(symbol))
+            {
+                binary = code.aCmdCode(parser2.symbol());
+                hackFile << binary << endl;
+            }
+            // Check if this symbol is a label which already been mapped.
+            else if (symbTable.contains(symbol))
+            {
+                memAddr = symbTable.getAddress(symbol);
+                binary = code.aCmdCode(to_string(memAddr));
+                hackFile << binary << endl;
+            }
+            // Then the symbol must be a variable.
+            else {
+                symbTable.addEntry(symbol, memOffset);
+                memOffset++;
+            }
         }
-        else if (parser.commandType() == C_COMMAND)
+        else if (parser2.commandType() == C_COMMAND)
         {
-            binary = "111" + code.comp(parser.comp()) + code.dest(parser.dest()) + code.jump(parser.jump());
+            binary = "111" + code.comp(parser2.comp()) + code.dest(parser2.dest()) + code.jump(parser2.jump());
             hackFile << binary << endl;
         }
     }
@@ -43,3 +83,4 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
